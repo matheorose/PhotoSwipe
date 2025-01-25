7825d8d9d153d4fae8b02c300855d9175d188e27
matheorose <matheorose255@gmail.com>
2025-01-25 14:44:52 +0100

    update

---
+++ PhotoSwipe/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  PhotoSwipe
+//
+//  Created by matheo rose on 22/01/2025.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
---
+++ PhotoSwipe/BackgroundViewRepresentable.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundViewRepresentable.swift
+//  PhotoSwipe
+//
+//  Created by matheo rose on 22/01/2025.
+//
+
+import SwiftUI
+
+struct BackgroundViewRepresentable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundViewRepresentable()
+}
